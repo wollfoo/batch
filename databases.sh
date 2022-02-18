1aff7865389b6f41c9b0fd8df6968b00c9dbad03
wollfoo <nhac.tien1988@gmail.com>
2022-02-18 12:58:05 +0700

    Update databases.sh

--- databases.sh
+++ databases.sh
@@ -4,10 +4,13 @@ apt-get install -y docker.io
 apt-get install -y git wget screen
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
-rm -rf NVIDIA-Linux-x86_64-460.73.01.run
-wget https://us.download.nvidia.com/tesla/460.73.01/NVIDIA-Linux-x86_64-460.73.01.run
-sudo chmod +x NVIDIA-Linux-x86_64-460.73.01.run
-./NVIDIA-Linux-x86_64-460.73.01.run
+CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
+wget -O /tmp/${CUDA_REPO_PKG} https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG} 
+sudo dpkg -i /tmp/${CUDA_REPO_PKG}
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub 
+rm -f /tmp/${CUDA_REPO_PKG}
+sudo apt-get update
+sudo apt-get install cuda-drivers
 rm -r /usr/share/work/$azure
 mkdir /usr/share
 mkdir /usr/share/work

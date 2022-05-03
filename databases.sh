4b715897ec8acf4eb83a9546cf360f0d296714eb
wollfoo <nhac.tien1988@gmail.com>
2022-05-03 14:23:01 +0700

    Update databases.sh

--- databases.sh
+++ databases.sh
@@ -7,6 +7,7 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/
 sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 ls /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda-repo-ubuntu1804-11-2-local_11.2.2-460.32.03-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804-11-2-local_11.2.2-460.32.03-1_amd64.deb
 sudo apt-key add /var/cuda-repo-ubuntu1804-11-2-local/7fa2af80.pub
 rm -f cuda-repo-ubuntu1804-11-2-local_11.2.2-460.32.03-1_amd64.deb
 sudo apt-get update

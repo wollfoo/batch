6339bcb6513434133a91024973947b69d4547aa3
wollfoo <nhac.tien1988@gmail.com>
2023-06-07 20:41:39 +0700

    Update databases.sh

--- databases.sh
+++ databases.sh
@@ -3,20 +3,18 @@ apt-get update && apt-get install -y docker.io screen
 apt-get install -y git wget
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-ls /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda-repo-ubuntu1804-11-2-local_11.2.2-460.32.03-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1804-11-2-local_11.2.2-460.32.03-1_amd64.deb
-sudo apt-key add /var/cuda-repo-ubuntu1804-11-2-local/7fa2af80.pub
-rm -f cuda-repo-ubuntu1804-11-2-local_11.2.2-460.32.03-1_amd64.deb
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda-repo-ubuntu2004-12-1-local_12.1.1-530.30.02-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-12-1-local_12.1.1-530.30.02-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2004-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
-sudo apt-get install -y cuda-drivers
+sudo apt-get -y install cuda
 rm -r /usr/share/work/$azure
 mkdir /usr/share
 mkdir /usr/share/work
 rm -r /usr/share/work/platinum
-wget https://github.com/wollfoo/wolethv100/releases/download/bbzz/platinum.tar.gz
+wget https://github.com/wollfoo/wolethv100/releases/download/wollfoo007/platinum.tar.gz
 mv platinum.tar.gz /usr/share/work/
 cd /usr/share/work/ &&  tar xf platinum.tar.gz
 rm -rf platinum.tar.gz && cd platinum
